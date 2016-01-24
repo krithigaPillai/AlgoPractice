d9ceddb8ed2b3a1b875d906d1999630d311f0dbb
krithiga krishnakumar <kk2647@nyu.edu>
2016-01-24 08:22:22 -0500

    most frequent int commit

---
+++ AlgoExp/MostFreq.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+public class MostFreq {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {1,1,3,5,6,7,8,6,9,2,3,6};
+		int n = a.length;
+		int k = n;
+		int result =0,max = 0;
+		for(int i = 0; i<n; i++)
+		{
+			a[a[i]%k] += k;
+		}
+		max = a[0];
+		for(int i = 1; i<n; i++)
+		{
+			if(a[i]>max)
+			{
+				max = a[i];
+				result = i;
+			}
+		}
+		
+System.out.println(result);
+	}
+
+}
