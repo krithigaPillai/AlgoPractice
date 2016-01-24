b5a02a499d757142e0050b63300ddc769037fbb5
krithiga krishnakumar <kk2647@nyu.edu>
2016-01-24 06:47:00 -0500

    insertion sort added

---
+++ AlgoExp/InsertionSort.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+
+/**
+ * @author krithigakrishnakumar
+ *
+ */
+import java.util.Scanner;
+public class InsertionSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner newInput = new Scanner(System.in);
+		System.out.println("enter number of elements:");
+		int n = newInput.nextInt();
+		int[] a = new int[n];
+		System.out.println("enter "+ n +"elements:");
+		for(int i=0;i<n;i++)
+		{
+			a[i] = newInput.nextInt();
+		}
+        insertionSort(a, n);
+	}
+	public static void insertionSort(int[] a, int n)
+	{
+		int j,temp;
+		for(int i=1; i<n; i++)
+		{
+			temp = a[i];
+			j=i-1;
+			while(j>=0 && a[j]>temp)
+			{
+				a[j+1] = a[j];
+				j=j-1;
+			}
+			a[j+1]=temp;
+		}
+		printArray(a, n);
+		
+	}
+	public static void printArray(int[] a,int n)
+	{
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+
+}
